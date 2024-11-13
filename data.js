3cdcba784d95dee29e40bc561d035fd01e890449
akromxolboyev2003 <akromxolboyev0615@gmail.com>
2024-11-13 18:16:43 +0500

    start

---
+++ data.js
@@ -0,0 +1,60 @@
+export let arr = [
+  {
+    name: "Alex Johnson",
+    job: "Product Designer",
+    email: "getemail@email.com",
+    phone: "+12 986 987 7867",
+    location: "37, Pollsatnd, New York, United State",
+    img: "./images/List.png",
+    id: 1,
+  },
+];
+
+export const newArr = [
+  {
+    img: "./images/post1.png",
+    name: "A life without the daily traffick",
+    year: "Post On 24.11.19",
+    title:
+      "It is a long established fact that a reader will bedistracted by the readable content of a page whenlooking at its layout",
+    text: "Read More",
+  },
+  {
+    img: "./images/post2.png",
+    name: "A life without the daily traffick",
+    year: "Post On 24.11.19",
+    title:
+      "It is a long established fact that a reader will bedistracted by the readable content of a page whenlooking at its layout",
+    text: "Read More",
+  },
+  {
+    img: "./images/post3.png",
+    name: "A life without the daily traffick",
+    year: "Post On 24.11.19",
+    title:
+      "It is a long established fact that a reader will bedistracted by the readable content of a page whenlooking at its layout",
+    text: "Read More",
+  },
+];
+
+
+export const aboutArr=[
+  {
+    img: "./images/client.png",
+    name: "Absolute wonderful ! I am completelyblown away.The very best.I was amazed atthe quality",
+    text: "John Mike",
+    title:"CEO, Author.Inc",
+  },
+  {
+    img: "./images/client.png",
+    name: "Absolute wonderful ! I am completelyblown away.The very best.I was amazed atthe quality",
+    text: "John Mike",
+    title:"CEO, Author.Inc",
+  },
+  {
+    img: "./images/client.png",
+    name: "Absolute wonderful ! I am completelyblown away.The very best.I was amazed atthe quality",
+    text: "John Mike",
+    title:"CEO, Author.Inc",
+  },
+]
